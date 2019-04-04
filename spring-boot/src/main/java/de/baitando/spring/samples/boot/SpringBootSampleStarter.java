2b50de4f99f96be9b17d038c29317216e70e8650
Andreas Hirsch <andreas.hirsch@mimacom.com>
2019-04-04 11:15:44 +0200

    Prepare Spring Boot sample

---
+++ spring-boot/src/main/java/de/baitando/spring/samples/boot/SpringBootSampleStarter.java
@@ -0,0 +1,22 @@
+package de.baitando.spring.samples.boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleStarter implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory.getLogger(SpringBootSampleStarter.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSampleStarter.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        LOG.info("Hello world!");
+    }
+}
---
+++ src/main/java/de/baitando/spring/samples/SpringSamplesApplication.java
@@ -0,0 +1,13 @@
+package de.baitando.spring.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSamplesApplication.class, args);
+    }
+
+}
