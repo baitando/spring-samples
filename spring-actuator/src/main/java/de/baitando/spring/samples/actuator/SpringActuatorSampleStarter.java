41124a245d050370092faa81e07bb19b5249e5ea
Andreas Hirsch <ando.hirsch@gmail.com>
2019-04-06 01:38:40 +0200

    Prepare Spring Actuator sample

---
+++ spring-actuator/src/main/java/de/baitando/spring/samples/actuator/SpringActuatorSampleStarter.java
@@ -0,0 +1,12 @@
+package de.baitando.spring.samples.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorSampleStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActuatorSampleStarter.class, args);
+    }
+}
