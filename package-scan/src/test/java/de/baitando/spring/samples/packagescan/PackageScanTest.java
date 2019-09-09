5a7ce254922d85f7d97e89261213b782bfaaf269
Andreas Hirsch <andreas.hirsch@mimacom.com>
2019-09-09 10:51:23 +0200

    Fix failing tests

--- package-scan/src/test/java/de/baitando/spring/samples/packagescan/PackageScanTest.java
+++ package-scan/src/test/java/de/baitando/spring/samples/packagescan/PackageScanTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -18,5 +20,11 @@ public class PackageScanTest {
     public void testContext() {
         System.out.println("Test service: " + testService);
         Assertions.assertThat(testService).isNotNull();
+    }
+
+    @Configuration
+    @ComponentScan
+    static class TestConfig {
+
     }
 }
--- spring-data-jpa/src/main/java/de/baitando/spring/samples/springdatajpa/repository/Account.java
+++ spring-data-jpa/src/main/java/de/baitando/spring/samples/springdatajpa/repository/Account.java
@@ -1,14 +1,16 @@
 package de.baitando.spring.samples.springdatajpa.repository;
 
 import javax.persistence.Embedded;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
 public class Account {
 
-    @EmbeddedId
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String iban;
