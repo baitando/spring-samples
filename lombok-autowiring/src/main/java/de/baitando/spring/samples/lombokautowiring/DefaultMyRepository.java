eee2b5ada97d225c4857c5a447b263c65590f1b6
Andreas Hirsch <andreas.hirsch@mimacom.com>
2019-11-10 15:14:25 +0100

    Fix project setup

--- lombok-autowiring/src/main/java/de/baitando/spring/samples/lombokautowiring/DefaultMyRepository.java
+++
@@ -1,7 +0,0 @@
-package de.baitando.spring.samples.lombokautowiring;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class DefaultMyRepository implements MyRepository {
-}
